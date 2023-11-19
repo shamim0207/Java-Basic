c8e6f6d5195f17e4f16a677555d1fd7535f1b9b4
shamim0207 <md.shamim8t8@gmail.com>
2023-11-19 18:56:02 +0600

    Change test project - name

--- test/src/Main.java
+++ test/src/Main.java
@@ -6,7 +6,7 @@ class Teacher {
 public class Main {
     public static void main(String[] args) {
         Teacher teacher1 = new Teacher();
-        teacher1.name = "shamim";
+        teacher1.name = "Md. Shamim";
         teacher1.gender = "male";
         System.out.println(teacher1.name);
         System.out.println(teacher1.gender);
